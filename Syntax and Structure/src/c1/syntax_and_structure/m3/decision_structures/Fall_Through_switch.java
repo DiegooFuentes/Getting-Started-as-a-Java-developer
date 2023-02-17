3a164d6986668c31622b4dabdefe3afaac1e11e1
DiegooFuentes <diegofuentesm123@gmail.com>
2023-02-17 18:00:09 -0300

    Fall-through Switch Statement

---
+++ Syntax and Structure/src/c1/syntax_and_structure/m3/decision_structures/Fall_Through_switch.java
@@ -0,0 +1,36 @@
+package c1.syntax_and_structure.m3.decision_structures;
+
+import java.util.Scanner;
+
+public class Fall_Through_switch {
+    public static void main(String[] args) {
+        //Fall-Through in Switch Statements
+        //Grade Message
+
+        System.out.println("Enter your letter grade:");
+        Scanner scanner = new Scanner(System.in);
+        String grade = scanner.next();
+        scanner.close();
+
+        //Fall-through will not stop until a break statement is reach.
+        //It can be used when you want the same message.
+        String message;
+        switch (grade) {
+            case "A":
+            case "B":
+                message = "Great job!";
+            case "C":
+                message = "Good job!";
+                break;
+            case  "D":
+            case  "F":
+                message = "You need to work a bit harder";
+                break;
+            default:
+                message = "Error. Invalid grade";
+                break;
+        }
+
+        System.out.println(message);
+    }
+}
