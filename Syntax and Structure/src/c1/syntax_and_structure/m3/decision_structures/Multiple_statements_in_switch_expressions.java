bb4d7c6d50ac96e0ba87e93deeac6b2db5467fa0
DiegooFuentes <diegofuentesm123@gmail.com>
2023-02-17 19:58:24 -0300

    Multiple statements in Switch expressions

---
+++ Syntax and Structure/src/c1/syntax_and_structure/m3/decision_structures/Multiple_statements_in_switch_expressions.java
@@ -0,0 +1,30 @@
+package c1.syntax_and_structure.m3.decision_structures;
+
+import java.util.Scanner;
+
+public class Multiple_statements_in_switch_expressions {
+    public static void main(String[] args) {
+
+        float firstNumber = 2;
+        float secondNumber = 8;
+        System.out.println("We have 2, and 8. Enter + or - to do the math.");
+        Scanner scanner = new Scanner(System.in);
+        String operation = scanner.next();
+
+        float result = switch (operation){
+            case "+" -> {
+                System.out.println("Adding...");
+                yield firstNumber + secondNumber;
+            }
+            case "-" -> {
+                System.out.println("Subtracting...");
+                yield firstNumber - secondNumber;
+            }
+            default -> {
+                throw new IllegalStateException("Invalid character");
+            }
+        };
+
+        System.out.println(result);
+    }
+}
