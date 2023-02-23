6f274ba86b8be19e346cdc667cc3a19b11e04d5a
DiegooFuentes <diegofuentesm123@gmail.com>
2023-02-22 22:59:16 -0300

    Methods java

---
+++ Syntax and Structure/src/c1/syntax_and_structure/m5/methods/Method.java
@@ -0,0 +1,21 @@
+package c1.syntax_and_structure.m5.methods;
+
+import java.util.Scanner;
+
+public class Method {
+    /*
+    * OUR FIRST METHOD
+    * Write a method that greets a user by name.
+    * */
+    public static void main(String[] args) {
+
+        greetUser();
+    }
+
+    public static void greetUser(){
+        System.out.print("Enter your name: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        System.out.println("Hi there, " + name);
+    }
+}
