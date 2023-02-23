83cdc2c7a81f9975af802e8d6f9c490aefede804
DiegooFuentes <diegofuentesm123@gmail.com>
2023-02-23 17:38:58 -0300

    Passing arguments to Methods java improved

--- Syntax and Structure/src/c1/syntax_and_structure/m5/methods/Passing_arguments_to_methods.java
+++ Syntax and Structure/src/c1/syntax_and_structure/m5/methods/Passing_arguments_to_methods.java
@@ -20,21 +20,30 @@ public class Passing_arguments_to_methods {
         int creditScore = scanner.nextInt();
         scanner.close();
 
-        isUserQualified(creditScore,salary);
-        //Check if the user is qualified
 
+        //Check if the user is qualified
+        boolean qualified = isUserQualified(creditScore,salary);
+        notifyUser(qualified);
     }
 
-    public static void isUserQualified(int creditScore,double salary){
+    public static boolean isUserQualified(int creditScore,double salary){
         double requiredSalary = 25000;
         int requiredCreditScore = 700;
 
         if(salary >= requiredSalary && creditScore >= requiredCreditScore){
-            System.out.println("Congrats! You're approved.");
+            return true;
         }
         else{
-            System.out.println("We are Sorry :( You've been declined.");
+            return false;
+        }
+    }
 
+    public static void notifyUser (boolean isQualified){
+        if(isQualified){
+            System.out.println("Congrats! You've been approved");
+        }
+        else{
+            System.out.println("Sorry. You've been declined");
         }
     }
 }
