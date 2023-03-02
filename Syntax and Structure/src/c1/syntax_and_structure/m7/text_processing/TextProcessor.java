900519dc4cbd2ba05fcf457c90efdd4bcdaf6629
DiegooFuentes <diegofuentesm123@gmail.com>
2023-03-02 19:43:24 -0300

    update

--- Syntax and Structure/src/c1/syntax_and_structure/m7/text_processing/TextProcessor.java
+++ Syntax and Structure/src/c1/syntax_and_structure/m7/text_processing/TextProcessor.java
@@ -13,13 +13,20 @@ public class TextProcessor {
     * */
     public static void countWord(String text){
 
-        String[] words = text.split(" ");
-        int numberOfWords = words.length;
-        String message = String.format("Your text contains %d words", numberOfWords);
+        String[] words = text.split(" "); //split the string using the whitespace as a divider
+        // split takes a delimiter which is what the string will be split on (whitespace)
+        // returns an array of strings, which is stored in the array of string variables named words
+
+        int numberOfWords = words.length; //Get the number of elements of the array (the number of words of the String)
+
+        String message = String.format("Your text contains %d words", numberOfWords); //format allows us to
+        // use placeholders (%d) within strings(%d is a placeholders for digits)(numberOfWords is the value of the placeholder)
+
         System.out.println(message);
 
+        //We use a for loop to run through the array
         for(int i =0; i<numberOfWords; i++){
-            System.out.println(words[i]);
+            System.out.println(words[i]); //Prints the array according to the index
         }
     }
 
