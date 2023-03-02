30bd165b2d4c83ba81bf5146cafa95fbfd814d5f
DiegooFuentes <diegofuentesm123@gmail.com>
2023-03-01 21:34:08 -0300

    update

--- Syntax and Structure/src/c1/syntax_and_structure/m6/objects/HomeAreaCalculatorWithMethods.java
+++ Syntax and Structure/src/c1/syntax_and_structure/m6/objects/HomeAreaCalculatorWithMethods.java
@@ -2,11 +2,11 @@ package c1.syntax_and_structure.m6.objects;
 
 public class HomeAreaCalculatorWithMethods {
     public static void main(String[] args) {
-        Rectangle kitchen = new Rectangle(200,400);
+        Rectangle kitchen = new Rectangle(200,400); //Rectangle constructor with parameters
         Rectangle bathroom = new Rectangle(300,700);
-        double area = calculateTotalArea(kitchen,bathroom);
+        double area = calculateTotalArea(kitchen,bathroom); // Passing objects as parameters to the method
 
-        System.out.println("The total area is: " + area);
+        System.out.println("The total area is: " + area); //printing
     }
 
     public static double calculateTotalArea(Rectangle rectangle1, Rectangle rectangle2){
