6f2b8f3fecd2442b3fbb390fce2c2c6d45f58d53
DiegooFuentes <diegofuentesm123@gmail.com>
2023-03-02 13:40:09 -0300

    Text processor count words

---
+++ Syntax and Structure/src/c1/syntax_and_structure/m7/text_processing/TextProcessor.java
@@ -0,0 +1,29 @@
+package c1.syntax_and_structure.m7.text_processing;
+
+public class TextProcessor {
+    public static void main(String[] args) {
+        countWord("I love Java");
+    }
+
+    /**
+    * Splits a String into an array by tokenizing it.
+    * Count words and prints them
+    * @param text Full string to be split
+    * */
+    public static void countWord(String text){
+
+        String[] words = text.split(" ");
+        int numberOfWords = words.length;
+        String message = String.format("Your text contains %d words", numberOfWords);
+        System.out.println(message);
+
+        for(int i =0; i<numberOfWords; i++){
+            System.out.println(words[i]);
+        }
+    }
+
+    // Prints a String in reverse order
+    public static void reverseString(String text){
+
+    }
+}
