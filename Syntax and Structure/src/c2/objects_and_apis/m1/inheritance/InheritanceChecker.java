899975b671bd4ea3d4af5bac8a43f76e88fa0467
DiegooFuentes <diegofuentesm123@gmail.com>
2023-03-09 13:20:02 -0300

    Inheritance Checker

---
+++ Syntax and Structure/src/c2/objects_and_apis/m1/inheritance/InheritanceChecker.java
@@ -0,0 +1,10 @@
+package c2.objects_and_apis.m1.inheritance;
+
+public class InheritanceChecker {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.getAge();
+        Employee employee = new Employee();
+
+    }
+}
